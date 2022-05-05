f8b0f8d66e2ea266c0a26d3e02781df65a4f4d78
pedram <mozafaripedram>
2022-05-05 19:56:01 +0430

    Initial Commit

---
+++ STDevExample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  STDevExample
+//
+//  Created by enigma 2 on 2/15/1401 AP.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
